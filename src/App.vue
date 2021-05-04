8465b230b16fc7bacc549886364bc57fcca4f3f8
quyennv <quyennv@vnext.com.vn>
2021-05-04 17:38:50 +0700

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <Editor/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Editor from './components/Editor.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Editor
   }
 }
 </script>
@@ -24,5 +25,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container {
+  width: 50%;
+  margin: 0 auto;
+  height: 800px;
 }
 </style>
---
+++ src/icons/Text.vue
@@ -0,0 +1,43 @@
+<template>
+  <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+       :width="width"
+       :height="height"
+       y="0px"
+       :aria-labelledby="iconName"
+       viewBox="0 0 405.333 405.333">
+    <g>
+      <g>
+        <g>
+          <polygon points="213.333,149.333 213.333,213.333 277.333,213.333 277.333,362.667 341.333,362.667 341.333,213.333
+				405.333,213.333 405.333,149.333"/>
+          <polygon points="277.333,42.667 0,42.667 0,106.667 106.667,106.667 106.667,362.667 170.667,362.667 170.667,106.667
+				277.333,106.667"/>
+        </g>
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    iconName: {
+      type: String,
+      default: 'box'
+    },
+    width: {
+      type: [Number, String],
+      default: 18
+    },
+    height: {
+      type: [Number, String],
+      default: 18
+    },
+    iconColor: {
+      type: String,
+      default: 'currentColor'
+    }
+  }
+}
+
+</script>
